35efdd0b04b555da4dbf81916e17e6599f84df15
Nicolas Cruz <143363153+PerformX2@users.noreply.github.com>
2025-10-17 13:55:17 -0400

    changement dans la scène (miel disparait)

--- projet_tp2_582301mo/scripts/honeybucket.gd
+++ projet_tp2_582301mo/scripts/honeybucket.gd
@@ -1,9 +1,13 @@
 extends Area2D
 @onready var anim = $AnimatedSprite2D
 
+var is_collected = false
 
 func _on_body_entered(body):
 	if body.name == "Player":
+		var is_collected = true 
+		$sfx_honey_pickedup.play()
+		
 		var tween = create_tween()
 
 		tween.tween_property(self, "position", position + Vector2(0,-30), 0.5)
